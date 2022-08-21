e6beebacc1abc01b3608e53f75d692e5ccfc82d0
Leo Souza <leonardodomaria@gmail.com>
2022-08-21 20:23:23 -0300

    crate user pt2

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express'
+import { userRouter } from './routes/user.routes'
 
 const app = express()
 
-app.get('/', (request, response) => {
-  return response.send('server on')
-})
+app.use(express.json())
+
+app.use(userRouter)
 
 app.listen(3333, () => console.log('Server is running on port 3333'))
