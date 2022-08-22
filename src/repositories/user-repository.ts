5629258898f13b5e23d9bbb83036cf62897319bb
Leo Souza <leonardodomaria@gmail.com>
2022-08-22 17:39:29 -0300

    create user

--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -1,7 +1,21 @@
 import { User } from '../entities/user-entity'
 
 export class UserRepository {
-  user: User[] = []
+  user: User[]
+
+  private static instance: UserRepository
+
+  constructor() {
+    this.user = []
+  }
+
+  static getInstance() {
+    if (!UserRepository.instance) {
+      UserRepository.instance = new UserRepository()
+    }
+
+    return UserRepository.instance
+  }
 
   async findByUsername(username: string) {
     return this.user.find((user) => user.username === username)
